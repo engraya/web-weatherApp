53a4abffcefed4b32f83ac9ae425e040f458a7d2
Engraya <ahmadyakubuaya@gmail.com>
2023-10-28 08:35:34 +0100

    Initial Commit

---
+++ src/Components/WeatherAppComponents/WeatherApp.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './WeatherApp.css'
+import searchIcon from '../Assets/search.png'
+
+const WeatherApp = () => {
+  return (
+    <>
+    <div className='starter'>
+
+    </div>
+    <div className='container'>
+    <div className="top-bar">
+    <input type="text" className="cityInput" placeholder='Search Cities' />
+    <div className="search-icon">
+        <img src="" alt="" />
+    </div>
+         
+    </div>
+      
+    </div>
+    </>
+  )
+}
+
+export default WeatherApp
